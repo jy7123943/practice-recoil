47e6bb27fdf7908103b5814866e10f9e4a43c88d
juyeon <juyeonh@rainist.com>
2020-12-11 23:28:42 +0900

    Add sorting function

--- src/feature/TodoController.tsx
+++ src/feature/TodoController.tsx
@@ -1,7 +1,21 @@
 import React, { useRef } from 'react';
 import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import { todoListState } from '../atoms';
+import { TodoListItem } from '../entity';
 import './TodoList.css';
+
+enum SORT_TYPE {
+  NEW = 'new',
+  OLD = 'old',
+}
+
+const sortTodoList = (list: TodoListItem[], sortType: SORT_TYPE) => (
+  [...list].sort((left, right) => (
+    sortType === SORT_TYPE.NEW
+      ? right.created_at - left.created_at
+      : left.created_at - right.created_at
+  ))
+);
 
 function TodoController() {
   const deleteAllItems = useResetRecoilState(todoListState);
@@ -14,12 +28,16 @@ function TodoController() {
     })));
   };
 
+  const sortItems = (sortType: SORT_TYPE) => {
+    setTodoList((oldTodoList) => sortTodoList(oldTodoList, sortType));
+  };
+
   return (
     <div>
       <button type='button' onClick={ setAllItemsDone }>ALL DONE</button>
       <button type='button' onClick={ deleteAllItems }>ALL DELETE</button>
-      <button type='button'>NEW</button>
-      <button type='button'>OLD</button>
+      <button type='button' onClick={ () => sortItems(SORT_TYPE.NEW) }>NEW</button>
+      <button type='button' onClick={ () => sortItems(SORT_TYPE.OLD) }>OLD</button>
     </div>
   );
 }
