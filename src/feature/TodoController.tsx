bdd21b2948a1fbac7ea4136665b752f783e19505
juyeon <juyeonh@rainist.com>
2020-12-11 23:16:54 +0900

    Add all items done function

--- src/feature/TodoController.tsx
+++ src/feature/TodoController.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { useResetRecoilState } from 'recoil';
+import React, { useRef } from 'react';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { todoListState } from '../atoms';
 import './TodoList.css';
 
 function TodoController() {
+  const deleteAllItems = useResetRecoilState(todoListState);
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const setAllItemsDone = () => {
+    setTodoList((oldTodoList) => oldTodoList.map((item) => ({
+      ...item,
+      is_done: true
+    })));
+  };
+
   return (
     <div>
-      <button type='button'>ALL CLEAR</button>
+      <button type='button' onClick={ setAllItemsDone }>ALL DONE</button>
+      <button type='button' onClick={ deleteAllItems }>ALL DELETE</button>
       <button type='button'>NEW</button>
       <button type='button'>OLD</button>
     </div>
