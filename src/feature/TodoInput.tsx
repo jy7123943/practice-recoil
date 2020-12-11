f19ba75acd4679db20d1035b6b59784fed9a473b
juyeon <juyeonh@rainist.com>
2020-12-11 20:28:17 +0900

    Add text atom

--- src/feature/TodoInput.tsx
+++ src/feature/TodoInput.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { useRecoilState } from 'recoil';
+import { todoTextState } from '../atoms';
 
 function TodoInput() {
+  const [todoText, setTodoText] = useRecoilState(todoTextState);
+
+  const onTodoTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTodoText(event.target.value);
+  };
+
   return (
     <div className="todo-input">
-      <input type='text' />
+      <input type='text' value={ todoText } onChange={ onTodoTextChange } />
       <button type='button'>SAVE</button>
     </div>
   );
