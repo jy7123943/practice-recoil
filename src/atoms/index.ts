edcf24fa7f7cd57cca32832f8a63d06707b6d0a8
juyeon <juyeonh@rainist.com>
2020-12-20 23:41:46 +0900

    Add activity atom

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -3,6 +3,7 @@ import { getTodoListFromStorage } from './../storage';
 import { atom } from 'recoil';
 import type { RecoilState } from 'recoil';
 import type { TodoListItem } from './../entity';
+import type { Activity } from '../entity/activity';
 
 export const todoTextState = atom({
   key: 'todoTextState',
@@ -22,4 +23,19 @@ export const todoListFilterState = atom({
 export const todoSortState = atom({
   key: 'todoSortState',
   default: getSortStateFromStorage(),
+});
+
+export const activityState = atom({
+  key: 'activityState',
+  default: {} as Activity,
+});
+
+export const activityTypeState = atom({
+  key: 'activityTypeState',
+  default: undefined,
+});
+
+export const refreshActivityState = atom({
+  key: 'refreshActivityState',
+  default: 0,
 });
