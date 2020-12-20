860788d52850532baef871cc2bb5c95d90f1c667
juyeon <juyeonh@rainist.com>
2020-12-21 00:52:57 +0900

    Add activity type filter component

---
+++ src/feature/Activity/ActivityTypeFilter.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { activityState, activityTypeState } from '../../atoms';
+import { ACTIVITY_TYPE } from '../../entity/activity';
+import './ActivityTypeFilter.css';
+
+function ActivityTypeFilter() {
+  const setActivityType = useSetRecoilState(activityTypeState);
+  const { type } = useRecoilValue(activityState);
+
+  return (
+    <div className='filter'>
+      { Object.values(ACTIVITY_TYPE).map((activityType) => (
+        <button
+          key={ activityType }
+          type='button'
+          onClick={ () => setActivityType(activityType) }
+          className={ type === activityType ? 'selected' : '' }
+        >
+          { activityType }
+        </button>
+      )) }
+    </div>
+  );
+}
+
+export default ActivityTypeFilter;
