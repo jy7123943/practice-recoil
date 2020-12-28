2dc4108ca727c5186be770acec1faf476a1fbabd
juyeon <juyeonh@rainist.com>
2020-12-28 16:06:43 +0900

    Connect to query client with provider

--- src/feature/App.tsx
+++ src/feature/App.tsx
@@ -3,17 +3,22 @@ import { Route, Switch } from 'react-router-dom';
 import Todo from './Todo';
 import Activity from './Activity';
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/'>
-        <Todo />
-      </Route>
-      <Route exact path='/activity'>
-        <Activity />
-      </Route>
-    </Switch>
+    <QueryClientProvider client={ queryClient }>
+      <Switch>
+        <Route exact path='/'>
+          <Todo />
+        </Route>
+        <Route exact path='/activity'>
+          <Activity />
+        </Route>
+      </Switch>
+    </QueryClientProvider>
   );
 }
 
